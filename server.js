0a3ec0e6b5969c6c6ee157bdfd95e9d6e6e8ba10
Evening <evening@mstars-lab3-10s-iMac.local>
2023-03-02 17:33:18 +0800

    connection mysql

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
 const usersRoute = require("./routes/users");
 const categoriesRoute = require("./routes/categories");
+
+//connection mysql
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "azure_db",
+});
 
 const port = 8000;
 const server = express();
@@ -11,6 +22,67 @@ server.use(express.json());
 
 server.use("/users", usersRoute);
 server.use("/categories", categoriesRoute);
+
+server.get("/", async (req, res) => {
+  connection.query(`SELECT * FROM azure_user`, (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
+      return;
+    }
+    res.status(200).json({ message: "amjilttai", data: result });
+  });
+});
+server.get("/:id", async (req, res) => {
+  connection.query(
+    `SELECT * FROM azure_user WHERE aid=${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+        return;
+      }
+      res
+        .status(200)
+        .json({ message: req.params.id + " idtai hereglegch", data: result });
+    }
+  );
+});
+server.put("/:id", async (req, res) => {
+  connection.query(
+    `UPDATE azure_user SET name = "${req.body.name}" , last_name = "${req.body.last_name}"  WHERE aid=${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+        return;
+      }
+      res.status(200).json({ message: "amjilttai soligdlo", data: result });
+    }
+  );
+});
+server.delete("/:id", async (req, res) => {
+  connection.query(
+    `DELETE FROM azure_user WHERE aid=${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+        return;
+      }
+      res.status(200).json({ message: "amjilttai ustgagdla", data: result });
+    }
+  );
+});
+
+server.post("/", async (req, res) => {
+  connection.query(
+    `INSERT INTO  azure_user VALUES(${req.body.aid} , "${req.body.name}" , "${req.body.last_name}" )`,
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+        return;
+      }
+      res.status(200).json({ message: "amjilttai nmegdle", data: result });
+    }
+  );
+});
 
 server.listen(port, () => {
   console.log(`server is running at ${port}`);
